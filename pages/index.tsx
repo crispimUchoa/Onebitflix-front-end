d980416c32316f85859f59b4630272b04bae780f
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-27 09:12:53 -0300

    inicio da configuração de components e pastas

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import styles from '../styles/HomeNoAuth.module.css';
 
 const HomeNoAuth = () => {
   return <>
-  <Head>HEAD</Head>
+  <Head>
+    <title>Onebitflix</title>
+    <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+    <meta property="og:title" content="Onebitflix" key="title"/>
+    <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"/>
+  </Head>
   <main></main>
   </>
 }

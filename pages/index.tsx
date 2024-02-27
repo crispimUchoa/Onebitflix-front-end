3224c28068fd39c7f2c41a7a8f2626a28a344447
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-27 08:55:27 -0300

    configuração do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import styles from '../styles/HomeNoAuth.module.css';
+
+const HomeNoAuth = () => {
+  return <>
+  <Head>HEAD</Head>
+  <main></main>
+  </>
+}
+
+export default HomeNoAuth

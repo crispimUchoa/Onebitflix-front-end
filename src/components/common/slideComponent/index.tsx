4da21ec84066fd4aa222abd60a70182bc22fae51
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-02-27 15:17:05 -0300

    feita estrutura de slides - slides component

---
+++ src/components/common/slideComponent/index.tsx
@@ -0,0 +1,28 @@
+//@ts-ignore
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import { CourseType } from '../../../services/courseService';
+import "@splidejs/splide/dist/css/splide.min.css";
+import SlideCard from '../slideCard';
+
+interface props{
+    course: CourseType[]
+}
+
+export default function SlideComponent({course}:props){
+    return <>
+    <div>
+        <Splide options={{
+            type: 'loop',
+            perPage: 4,
+            perMove: 1,
+            pagination: false
+        }}>
+            {course?.map((course)=>(
+                <SplideSlide key={course.id}>
+                    <SlideCard course={course}></SlideCard>
+                </SplideSlide>
+            ))}
+        </Splide>
+    </div>
+    </>
+}
